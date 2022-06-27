17100927381c52d7540fb4bf6f2f1f1757b324a2
Ahmad Aminullah <emonyhwh@gmail.com>
2022-06-27 22:28:18 +0700

    try deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './component/navbar/navbar';
-import SlideOne from './component/homepage/sectionOne/slideone'
+import SlideOne from './component/homepage/sectionOne/slideone';
+import SlideTwo from './component/homepage/sectionTwo/slidetwo';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <SlideOne />
+      <SlideTwo />
     </div>
   );
 }
---
+++ src/component/homepage/sectionTwo/slidetwo.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./slidetwo.css";
+import trit from './tridtwo.svg';
+
+class SlideTwo extends Component {
+    render() {
+        return (
+            <div className='scndSect'>
+                <h1 className='sectHeader'>Fakultas di UI</h1>
+                <div className='contleft'>
+                    {/* <img src={trit} className='tridtwo' alt='stand-sally'/> */}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SlideTwo
